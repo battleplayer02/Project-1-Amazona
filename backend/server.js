5d21b44f328122d5befa0b74f0390b8463625d88
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-04-27 23:59:19 +0530

    a

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb+srv://himanshu:12345678@cluster0.6ftkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+
+
+
+
+
+await mongoose.connect('mongodb+srv://himanshu:XAfSuZuAiEwEOyEB@cluster0.6ftkm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
