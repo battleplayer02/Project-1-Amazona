1b2b31e39e422c3b1d7242ff6695f32cf0afa894
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-04-28 15:58:10 +0530

    a

---
+++ frontend/build/precache-manifest.1b3426b14851fe15d8fe7db4ac00d94f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1bac39c952d7e04758f4c0c26e2ba7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2d8e5dcd15276ebc7ea",
+    "url": "/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "19c09f359942ab1d7877",
+    "url": "/static/css/main.e69e3f7a.chunk.css"
+  },
+  {
+    "revision": "e2d8e5dcd15276ebc7ea",
+    "url": "/static/js/2.8d32616e.chunk.js"
+  },
+  {
+    "revision": "9b9de423b52b798b8fc649e7d9e276fb",
+    "url": "/static/js/2.8d32616e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19c09f359942ab1d7877",
+    "url": "/static/js/main.b8e30dd0.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
