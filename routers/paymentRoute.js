049a192e82747351349bf28deb808fecea949e0c
Himanshu Shekhar <42701850+battleplayer02@users.noreply.github.com>
2021-05-15 12:42:42 +0530

    a

--- routers/paymentRoute.js
+++ routers/paymentRoute.js
@@ -15,7 +15,7 @@ paymentRouter.post('/api/config/paytm', (req, res) => {
     params['ORDER_ID'] = orderid
     params['CUST_ID'] = "123"
     params['TXN_AMOUNT'] = "1.00"
-    params['CALLBACK_URL'] = 'http://https://amazona-himanshu.herokuapp.com/callback'
+    params['CALLBACK_URL'] = 'https://amazona-himanshu.herokuapp.com/callback'
     params['EMAIL'] = email
     params['MOBILE_NO'] = '9690299790'
 
